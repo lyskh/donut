ca7ab4c7a62dde4b7a5c2e965a829f98b5cc5f39
Geovan Genolos <142429957+GeovanGenolos@users.noreply.github.com>
2024-05-26 23:57:03 +0800

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -8,7 +8,7 @@ $database = "donut_db";
 // Create connection
 $connection = new mysqli($servername, $username, $password, $database);
 
-// Check connection
+// Connection check
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
